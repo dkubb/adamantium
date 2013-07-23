139d6fcccb321c1b1a7449a5a8642b8c87b689db
Dan Kubb <dan.kubb@gmail.com>
2013-07-23 09:07:18 -0700

    Add simplecov configuration

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,15 +1,28 @@
 # encoding: utf-8
 
-require 'adamantium'
 require 'devtools/spec_helper'
 
-RSpec.configure do |config|
+if ENV['COVERAGE'] == 'true'
+  require 'simplecov'
+  require 'coveralls'
+
+  SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
+    SimpleCov::Formatter::HTMLFormatter,
+    Coveralls::SimpleCov::Formatter
+  ]
+
+  SimpleCov.start do
+    command_name     'spec:unit'
+    add_filter       'config'
+    add_filter       'spec'
+    minimum_coverage 100
+  end
 end
 
-if RUBY_VERSION >= '1.9' and ENV['COVERAGE'] == 'true'
-  require 'simplecov'
-  SimpleCov.start do
-    command_name 'spec:unit'
-    add_filter   'spec'
+require 'adamantium'
+
+RSpec.configure do |config|
+  config.expect_with :rspec do |expect_with|
+    expect_with.syntax = [:should, :expect]
   end
 end
