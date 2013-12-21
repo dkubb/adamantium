33654f25a9a120babf4c2431dfa2c998579e9bd6
Dan Kubb <dan.kubb@gmail.com>
2013-12-20 23:25:39 -0800

    Add vendor to the list of paths for simplecov to ignore

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -12,9 +12,12 @@ if ENV['COVERAGE'] == 'true'
   ]
 
   SimpleCov.start do
-    command_name     'spec:unit'
-    add_filter       'config'
-    add_filter       'spec'
+    command_name 'spec:unit'
+
+    add_filter 'config'
+    add_filter 'spec'
+    add_filter 'vendor'
+
     minimum_coverage 100
   end
 end
